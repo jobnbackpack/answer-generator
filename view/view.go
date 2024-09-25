12b78dd1c2a3ddc43ef15df27f20b1cdbacc388d
Robin <rjanzen@conet.de>
2024-09-25 08:53:47 +0200

    fix: render list correctly

--- view/view.go
+++ view/view.go
@@ -1,14 +1,22 @@
 package view
 
 import (
+	"fmt"
+	"io"
 	"log"
+	"strings"
 
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
-var docStyle = lipgloss.NewStyle().Margin(1, 2)
+var (
+	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
+	docStyle          = lipgloss.NewStyle().Margin(1, 2)
+	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+)
 
 type Choice struct {
 	Value   string `json:"value"`
@@ -23,6 +31,29 @@ func (c Choice) Title() string {
 	return c.Value
 }
 
+type itemDelegate struct{}
+
+func (d itemDelegate) Height() int                             { return 1 }
+func (d itemDelegate) Spacing() int                            { return 0 }
+func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(Choice)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s", index+1, i.Title())
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s ...string) string {
+			return selectedItemStyle.Render("> " + strings.Join(s, " "))
+		}
+	}
+
+	fmt.Fprint(w, fn(str))
+}
+
 type model struct {
 	list list.Model
 }
@@ -34,10 +65,14 @@ func CreateView(question string, choiceResult []Choice) model {
 		choiceResult[0],
 		choiceResult[1],
 		choiceResult[2],
+		choiceResult[3],
 	}
 
-	newList := list.New(items, list.NewDefaultDelegate(), 0, 0)
+	newList := list.New(items, itemDelegate{}, 0, 0)
 	newList.Title = question
+	newList.SetShowStatusBar(false)
+	newList.SetFilteringEnabled(false)
+	newList.Styles.Title = titleStyle
 
 	return model{
 		list: newList,
